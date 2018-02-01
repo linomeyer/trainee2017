9a0a6498cd729e976c4368c4df5fd22fae2bfa69
Lino Meyer <linomeyer02@gmail.com>
2018-02-01 10:25:39 +0100

    added mistake counter

--- Chapter07/src/InputTimer.java
+++ Chapter07/src/InputTimer.java
@@ -6,18 +6,30 @@ import javax.swing.JOptionPane;
 public class InputTimer {
   public static void main(String[] args) {
     String correctAnswer = "Fischers Fritz fischt frische Fische";
+
     Instant startTime = Instant.now();
     String input = JOptionPane.showInputDialog("Geben Sie folgenden Satz ein: \n Fischers Fritz fischt frische Fische");
     Instant endTime = Instant.now();
+
     Duration duration = Duration.between(startTime, endTime);
     Long millis = duration.toMillis() - duration.getSeconds() * 1000;
+
+    int amountOfMistakes = 0;
+    for (int x = 0; x < input.length(); x++) {
+      if (input.charAt(x) == correctAnswer.charAt(x)) {
+      } else {
+        amountOfMistakes++;
+      }
+    }
+    // If input is shorter than the correctAnswer add mistakes
+    amountOfMistakes += correctAnswer.length() - input.length();
 
     if (input.equals(correctAnswer)) {
       JOptionPane.showMessageDialog(null, "Fehlerfrei! " + duration.toMinutes() + " Minuten, " + duration.getSeconds()
           + " Sekunden, " + millis + " Milisekunden");
     } else {
-      JOptionPane.showMessageDialog(null, "Es hat noch Fehler! " + duration.toMinutes() + " Minuten, "
-          + duration.getSeconds() + " Sekunden, " + millis + " Milisekunden");
+      JOptionPane.showMessageDialog(null, "Es hat noch " + amountOfMistakes + " Fehler! " + duration.toMinutes()
+          + " Minuten, " + duration.getSeconds() + " Sekunden, " + millis + " Milisekunden");
     }
   }
 }
