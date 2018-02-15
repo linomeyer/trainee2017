68dac7b0bbddf8d6317942a5c0a25d679fc5f6c9
Lars Ghezzi <l.ghezzi@bluewin.ch>
2018-02-15 13:55:22 +0100

    started to change StringSorter

--- Chapter07/src_test/StringSorterTest.java
+++ Chapter07/src_test/StringSorterTest.java
@@ -10,12 +10,30 @@ public class StringSorterTest {
     String str1 = "Lino";
     String str2 = "Lars";
     String str3 = "Luca";
+    String[] strings = { str1, str2, str3 };
 
-    String[] sortedStrings = StringSorter.sort(str1, str2, str3);
+    String[] sortedStrings = StringSorter.sort(strings);
 
     assertThat(sortedStrings[0], is("Lars"));
     assertThat(sortedStrings[1], is("Lino"));
     assertThat(sortedStrings[2], is("Luca"));
   }
 
+  @Test
+  public void fiveStrings_sort_threeStringsSorted() throws Exception {
+    String str1 = "Silvan";
+    String str2 = "Lars";
+    String str3 = "Reto";
+    String str4 = "Luca";
+    String str5 = "Lino";
+    String[] strings = { str1, str2, str3, str4, str5 };
+
+    String[] sortedStrings = StringSorter.sort(strings);
+
+    assertThat(sortedStrings[0], is("Lars"));
+    assertThat(sortedStrings[1], is("Lino"));
+    assertThat(sortedStrings[2], is("Luca"));
+    assertThat(sortedStrings[3], is("Reto"));
+    assertThat(sortedStrings[4], is("Silvan"));
+  }
 }
