63337e1a2c77ae6397c7fbe937584299611ca32a
Lino Meyer <linomeyer02@gmail.com>
2018-02-22 11:45:22 +0100

    added Time class

--- Chapter09/src/tech/bison/tranee17/chapter09/Time.java
+++ Chapter09/src/tech/bison/tranee17/chapter09/Time.java
@@ -1,5 +1,31 @@
 package tech.bison.tranee17.chapter09;
 
-public class Time {
+import javax.swing.JOptionPane;
 
+public class Time {
+  public static void main(String[] args) {
+    int hours;
+    int minutes;
+    String time;
+
+    String strHours = JOptionPane.showInputDialog("Geben Sie die Stunde ein: ");
+    String strMinutes = JOptionPane.showInputDialog("Geben Sie die Minute ein: ");
+    hours = Integer.parseInt(strHours);
+    minutes = Integer.parseInt(strMinutes);
+
+    time = toTime(hours, minutes);
+
+    JOptionPane.showMessageDialog(null, time);
+  }
+
+  public static String toTime(int hours, int minutes) {
+    String time;
+    try {
+      time = "Ihre Uhrzeit ist " + hours + ":" + minutes + "Uhr";
+    } catch (NumberFormatException e) {
+      time = "Die eingegebenen Zahlen sind ungültig!";
+    }
+    return time;
+
+  }
 }
--- Chapter09/src_test/tech/bison/tranee17/chapter09/TimeTest.java
+++
@@ -1,9 +0,0 @@
-package tech.bison.tranee17.chapter09;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-public class TimeTest {
-
-}
