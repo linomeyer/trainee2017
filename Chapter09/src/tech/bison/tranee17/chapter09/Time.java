7a94fd97b8ee969fbb6b9e3783374b6eb7f86f60
Lino Meyer <linomeyer02@gmail.com>
2018-02-22 12:03:07 +0100

    Time is working now

--- Chapter09/src/tech/bison/tranee17/chapter09/Time.java
+++ Chapter09/src/tech/bison/tranee17/chapter09/Time.java
@@ -4,28 +4,21 @@ import javax.swing.JOptionPane;
 
 public class Time {
   public static void main(String[] args) {
+    String time;
     int hours;
     int minutes;
-    String time;
 
     String strHours = JOptionPane.showInputDialog("Geben Sie die Stunde ein: ");
     String strMinutes = JOptionPane.showInputDialog("Geben Sie die Minute ein: ");
-    hours = Integer.parseInt(strHours);
-    minutes = Integer.parseInt(strMinutes);
 
-    time = toTime(hours, minutes);
+    try {
+      hours = Integer.parseInt(strHours);
+      minutes = Integer.parseInt(strMinutes);
+      time = "Ihre Uhrzeit ist " + hours + ":" + minutes + " Uhr!";
+    } catch (NumberFormatException e) {
+      time = "Die Eingabe war ungültig.";
+    }
 
     JOptionPane.showMessageDialog(null, time);
-  }
-
-  public static String toTime(int hours, int minutes) {
-    String time;
-    try {
-      time = "Ihre Uhrzeit ist " + hours + ":" + minutes + "Uhr";
-    } catch (NumberFormatException e) {
-      time = "Die eingegebenen Zahlen sind ungültig!";
-    }
-    return time;
-
   }
 }
