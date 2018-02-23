5197969f8a63e3cc9c274a16b835edc9d03f1b60
Luca Bucher <lucbu01@bluewin.ch>
2018-02-23 08:51:10 +0100

    added serialVersionUID

--- Chapter09/src/tech/bison/tranee17/chapter09/TimeFormatException.java
+++ Chapter09/src/tech/bison/tranee17/chapter09/TimeFormatException.java
@@ -2,6 +2,11 @@ package tech.bison.tranee17.chapter09;
 
 public class TimeFormatException extends NumberFormatException {
 
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
   public TimeFormatException() {
     super();
   }
