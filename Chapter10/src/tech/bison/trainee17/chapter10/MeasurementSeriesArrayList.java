54d3187047c7eda3f9477e82ee3f73d980617864
Lino Meyer <linomeyer02@gmail.com>
2018-03-02 09:15:01 +0100

    added MeasurementSeriesArrayList

--- Chapter10/src/tech/bison/trainee17/chapter10/MeasurementSeriesArrayList.java
+++ Chapter10/src/tech/bison/trainee17/chapter10/MeasurementSeriesArrayList.java
@@ -1,5 +1,45 @@
 package tech.bison.trainee17.chapter10;
 
-public class MeasurementSeriesArrayList {
+import java.util.ArrayList;
 
+public class MeasurementSeriesArrayList extends ArrayList<Double> {
+
+  private static final long serialVersionUID = 1L;
+
+  public MeasurementSeriesArrayList() {
+    super();
+  }
+
+  public Evaluation evaluate() {
+    return new Evaluation(highestValue(), lowestValue(), averageValue());
+  }
+
+  private double averageValue() {
+    int length = this.size();
+    double averageValue = 0;
+    for (int x = length - 1; x >= 0; x--) {
+      averageValue += this.get(x);
+    }
+    return averageValue / length;
+  }
+
+  private double highestValue() {
+    double highestValue = this.get(0);
+    int length = this.size();
+    for (int x = 0; x < length; x++) {
+      if (this.get(x) > highestValue)
+        highestValue = this.get(x);
+    }
+    return highestValue;
+  }
+
+  private double lowestValue() {
+    double lowestValue = this.get(0);
+    int length = this.size();
+    for (int x = 0; x < length; x++) {
+      if (this.get(x) < lowestValue)
+        lowestValue = this.get(x);
+    }
+    return lowestValue;
+  }
 }
--- Chapter10/src_test/tech/bison/trainee17/chapter10/MeasurementSeriesArrayListTest.java
+++ Chapter10/src_test/tech/bison/trainee17/chapter10/MeasurementSeriesArrayListTest.java
@@ -1,5 +1,38 @@
 package tech.bison.trainee17.chapter10;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
 public class MeasurementSeriesArrayListTest {
+  @Test
+  public void arrayList_addValues_EvaluatedValues() throws Exception {
+    MeasurementSeriesArrayList arrayList = new MeasurementSeriesArrayList();
+
+    arrayList.addAll(Arrays.asList(12.0, 45.3, 78.2, 45.8, 56.6));
+
+    Evaluation result = arrayList.evaluate();
+
+    assertThat(Math.round(result.averageValue * 100) / 100.0, is(47.58));
+    assertThat(Math.round(result.highestValue * 100) / 100.0, is(78.2));
+    assertThat(Math.round(result.lowestValue * 100) / 100.0, is(12.0));
+  }
+
+  @Test
+  public void arrayList_removeValues_arrayWithoutRemovedObjects() throws Exception {
+    MeasurementSeriesArrayList arrayList = new MeasurementSeriesArrayList();
+
+    arrayList.addAll(Arrays.asList(12.56, 45.2, 78.6, 15.3, 89.6));
+    arrayList.remove(0);
+    arrayList.remove(3);
+
+    MeasurementSeriesArrayList arrayListWithoutRemovedObjects = new MeasurementSeriesArrayList();
+    arrayListWithoutRemovedObjects.addAll(Arrays.asList(45.2, 78.6, 15.3));
+
+    assertThat(arrayList, is(arrayListWithoutRemovedObjects));
+  }
 
 }
