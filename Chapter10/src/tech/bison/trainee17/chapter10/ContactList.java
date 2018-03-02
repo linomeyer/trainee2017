d883d8945592d181509c46b8f22890d3d82ae0d9
Lino Meyer <linomeyer02@gmail.com>
2018-03-02 11:08:06 +0100

    added test to remove Contacts

--- Chapter10/src/tech/bison/trainee17/chapter10/ContactList.java
+++ Chapter10/src/tech/bison/trainee17/chapter10/ContactList.java
@@ -4,4 +4,6 @@ import java.util.ArrayList;
 
 public class ContactList extends ArrayList<Contact> {
 
+  private static final long serialVersionUID = 1L;
+
 }
--- Chapter10/src_test/tech/bison/trainee17/chapter10/ContactListTest.java
+++ Chapter10/src_test/tech/bison/trainee17/chapter10/ContactListTest.java
@@ -16,4 +16,24 @@ public class ContactListTest {
 
     assertThat(contactList.get(0), is(lino));
   }
+
+  @Test
+  public void contactList_removeContacts_contactListWithoutRemovedContacts() throws Exception {
+    Contact lino = new Contact("Lino", "Meyer", "Eichenstrasse 11", "6023", "Rothenburg", "lino.meyer@bluewin.ch",
+        "+41 76 558 20 90");
+    Contact luca = new Contact("Luca", "Bucher", "Scheid 1", "6026", "Rain", "lucbu01@bluewin.ch", "+41 79 664 10 72");
+    Contact lars = new Contact("Lars", "Ghezzi", "Geuenseerstrasse 16d", "6210", "Sursee", "lars.ghezzi@bluewin.ch",
+        "+41 79 608 91 35");
+
+    ContactList contactList = new ContactList();
+
+    contactList.add(lino);
+    contactList.add(luca);
+    contactList.add(lars);
+
+    contactList.remove(0);
+    contactList.remove(luca);
+
+    assertThat(contactList.get(0), is(lars));
+  }
 }
