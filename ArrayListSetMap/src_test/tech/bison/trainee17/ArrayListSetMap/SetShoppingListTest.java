6246c59f43e7a6390c24e965e1f20acbe465a89e
luca.bucher <luca.bucher@FENNB00392.main.corp.fenaco.com>
2018-03-23 13:39:36 +0100

    remove Items on Set

--- ArrayListSetMap/src_test/tech/bison/trainee17/ArrayListSetMap/SetShoppingListTest.java
+++ ArrayListSetMap/src_test/tech/bison/trainee17/ArrayListSetMap/SetShoppingListTest.java
@@ -16,9 +16,26 @@ public class SetShoppingListTest {
     shoppingList.add("apple");
     shoppingList.add("banana");
     shoppingList.add("apple");
-    shoppingList.add("strwaberry");
+    shoppingList.add("strawberry");
 
     assertThat(shoppingList.size(), is(4));
     assertThat(shoppingList.contains("lemon"), is(true));
+    assertThat(shoppingList.contains("apple"), is(true));
+    assertThat(shoppingList.contains("banana"), is(true));
+    assertThat(shoppingList.contains("strawberry"), is(true));
+  }
+
+  @Test
+  public void createShoppingList_addValues_removeValues() throws Exception {
+    HashSet<String> shoppingList = new HashSet<String>();
+
+    shoppingList.add("lemon");
+    shoppingList.add("apple");
+    shoppingList.add("banana");
+    shoppingList.add("strawberry");
+
+    shoppingList.remove("apple");
+
+    assertThat(shoppingList.contains("apple"), is(false));
   }
 }
