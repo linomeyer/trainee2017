571146fedd6569c385385267f41aa9de3e1ff0bc
Lars Ghezzi <l.ghezzi@bluewin.ch>
2018-04-25 14:37:06 +0200

    new KingTest

--- Bi-Chess/src_test/tech/bison/trainee2017/chess/KingTest.java
+++ Bi-Chess/src_test/tech/bison/trainee2017/chess/KingTest.java
@@ -5,32 +5,84 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
-import tech.bison.trainee2017.chess.King;
-
 public class KingTest {
   @Test
-  public void king_startAndEndPosition_validMove() throws Exception {
+  public void king_isAllowedToMoveOneUp_validMove() throws Exception {
     King king = new King();
 
-    boolean validMove = king.isAValidMove("c2", "d3");
+    boolean validMove = king.isAValidMove("a2", "a3");
 
     assertThat(validMove, is(true));
   }
 
   @Test
-  public void king_startAndEndPositionAreSimilar_invalidMove() throws Exception {
+  public void king_isAllowedToMoveOneDown_validMove() throws Exception {
     King king = new King();
 
-    boolean validMove = king.isAValidMove("c2", "c2");
+    boolean validMove = king.isAValidMove("c3", "c2");
+
+    assertThat(validMove, is(true));
+  }
+
+  @Test
+  public void king_isAllowedToMoveOneRight_validMove() throws Exception {
+    King king = new King();
+
+    boolean validMove = king.isAValidMove("e5", "f5");
+
+    assertThat(validMove, is(true));
+  }
+
+  @Test
+  public void king_isAllowedToMoveOneLeft_validMove() throws Exception {
+    King king = new King();
+
+    boolean validMove = king.isAValidMove("e8", "d8");
+
+    assertThat(validMove, is(true));
+  }
+
+  @Test
+  public void king_isAllowedToMoveOneDiagonal_validMove() throws Exception {
+    King king = new King();
+
+    boolean validMove = king.isAValidMove("a2", "b3");
+
+    assertThat(validMove, is(true));
+  }
+
+  @Test
+  public void king_isAllowedToMoveTwoDiagonal_invalidMove() throws Exception {
+    King king = new King();
+
+    boolean validMove = king.isAValidMove("a2", "c4");
 
     assertThat(validMove, is(false));
   }
 
   @Test
-  public void king_startAndEndPositionOutOfField_invalidMove() throws Exception {
+  public void king_isAllowedToMoveTwoUp_invalidMove() throws Exception {
     King king = new King();
 
-    boolean validMove = king.isAValidMove("h8", "i9");
+    boolean validMove = king.isAValidMove("d5", "d7");
+
+    assertThat(validMove, is(false));
+  }
+
+  @Test
+  public void king_isAllowedToMoveTwoLeft_invalidMove() throws Exception {
+    King king = new King();
+
+    boolean validMove = king.isAValidMove("f8", "d8");
+
+    assertThat(validMove, is(false));
+  }
+
+  @Test
+  public void king_isAllowedToMoveWetherHorizontalVerticalOrDiagonal_invalidMove() throws Exception {
+    King king = new King();
+
+    boolean validMove = king.isAValidMove("c2", "d4");
 
     assertThat(validMove, is(false));
   }
