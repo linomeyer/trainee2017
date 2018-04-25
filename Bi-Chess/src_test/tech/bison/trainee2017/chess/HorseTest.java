f81c7838efb7d873c5c715c857a3f082019621ae
Luca Bucher <lucbu01@bluewin.ch>
2018-04-25 14:56:33 +0200

    Reengeniering of HorseTest

--- Bi-Chess/src_test/tech/bison/trainee2017/chess/HorseTest.java
+++ Bi-Chess/src_test/tech/bison/trainee2017/chess/HorseTest.java
@@ -5,43 +5,94 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
-import tech.bison.trainee2017.chess.Horse;
-
 public class HorseTest {
   @Test
-  public void knight_validMovement_validMove() throws Exception {
-    Horse knight = new Horse();
+  public void horse_isAllowedToMoveTwoUpAndOneRight_validMove() throws Exception {
+    Horse horse = new Horse();
 
-    boolean validMove = knight.isAValidMove("b1", "c3");
-
-    assertThat(validMove, is(true));
-  }
-
-  @Test
-  public void knight_validMovementWithMinusValue_validMove() throws Exception {
-    Horse knight = new Horse();
-
-    boolean validMove = knight.isAValidMove("b8", "a6");
+    boolean validMove = horse.isAValidMove("D5", "E7");
 
     assertThat(validMove, is(true));
   }
 
   @Test
-  public void knight_horizontalMovement_invalidMove() throws Exception {
-    Horse knight = new Horse();
+  public void horse_isAllowedToMoveTwoUpAndOneLeft_validMove() throws Exception {
+    Horse horse = new Horse();
 
-    boolean validMove = knight.isAValidMove("b1", "b3");
+    boolean validMove = horse.isAValidMove("D5", "C7");
+
+    assertThat(validMove, is(true));
+  }
+
+  @Test
+  public void horse_isAllowedToMoveTwoRightAndOneUp_validMove() throws Exception {
+    Horse horse = new Horse();
+
+    boolean validMove = horse.isAValidMove("D5", "F6");
+
+    assertThat(validMove, is(true));
+  }
+
+  @Test
+  public void horse_isAllowedToMoveTwoRightAndOneDown_validMove() throws Exception {
+    Horse horse = new Horse();
+
+    boolean validMove = horse.isAValidMove("D5", "F4");
+
+    assertThat(validMove, is(true));
+  }
+
+  @Test
+  public void horse_isAllowedToMoveTwoDownAndOneRight_validMove() throws Exception {
+    Horse horse = new Horse();
+
+    boolean validMove = horse.isAValidMove("D5", "E3");
+
+    assertThat(validMove, is(true));
+  }
+
+  @Test
+  public void horse_isAllowedToMoveTwoDownAndOneLeft_validMove() throws Exception {
+    Horse horse = new Horse();
+
+    boolean validMove = horse.isAValidMove("D5", "C3");
+
+    assertThat(validMove, is(true));
+  }
+
+  @Test
+  public void horse_isAllowedToMoveTwoLeftAndOneUp_validMove() throws Exception {
+    Horse horse = new Horse();
+
+    boolean validMove = horse.isAValidMove("D5", "B6");
+
+    assertThat(validMove, is(true));
+  }
+
+  @Test
+  public void horse_isAllowedToMoveTwoLeftAndOneDown_validMove() throws Exception {
+    Horse horse = new Horse();
+
+    boolean validMove = horse.isAValidMove("D5", "B4");
+
+    assertThat(validMove, is(true));
+  }
+
+  @Test
+  public void horse_isAllowedToMoveTwoUpAndTwoRight_invalidMove() throws Exception {
+    Horse horse = new Horse();
+
+    boolean validMove = horse.isAValidMove("D5", "F7");
 
     assertThat(validMove, is(false));
   }
 
   @Test
-  public void knight_verticalMovement_validMove() throws Exception {
-    Horse knight = new Horse();
+  public void horse_isAllowedToMoveTwoDownAndTwoRight_invalidMove() throws Exception {
+    Horse horse = new Horse();
 
-    boolean validMove = knight.isAValidMove("c2", "e2");
+    boolean validMove = horse.isAValidMove("D5", "F3");
 
     assertThat(validMove, is(false));
   }
-
 }
