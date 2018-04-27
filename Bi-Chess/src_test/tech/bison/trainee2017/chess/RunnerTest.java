1970b2a86906db83aabdbe899007212a90ad6164
Lars Ghezzi <l.ghezzi@bluewin.ch>
2018-04-27 11:29:58 +0200

    follow style guide

--- Bi-Chess/src_test/tech/bison/trainee2017/chess/RunnerTest.java
+++ Bi-Chess/src_test/tech/bison/trainee2017/chess/RunnerTest.java
@@ -7,82 +7,82 @@ import org.junit.Test;
 
 public class RunnerTest {
   @Test
-  public void Runner_isAllowedToMoveOneUp_invalidMove() throws Exception {
-    Runner Runner = new Runner();
+  public void runner_isAllowedToMoveOneUp_invalidMove() throws Exception {
+    Runner runner = new Runner();
 
-    boolean validMove = Runner.isAValidMove("c2", "c3");
-
-    assertThat(validMove, is(false));
-  }
-
-  @Test
-  public void Runner_isAllowedToMoveOneDown_invalidMove() throws Exception {
-    Runner Runner = new Runner();
-
-    boolean validMove = Runner.isAValidMove("h3", "h2");
+    boolean validMove = runner.isAValidMove("c2", "c3");
 
     assertThat(validMove, is(false));
   }
 
   @Test
-  public void Runner_isAllowedToMoveOneRight_invalidMove() throws Exception {
-    Runner Runner = new Runner();
+  public void runner_isAllowedToMoveOneDown_invalidMove() throws Exception {
+    Runner runner = new Runner();
 
-    boolean validMove = Runner.isAValidMove("e5", "f5");
-
-    assertThat(validMove, is(false));
-  }
-
-  @Test
-  public void Runner_isAllowedToMoveOneLeft_invalidMove() throws Exception {
-    Runner Runner = new Runner();
-
-    boolean validMove = Runner.isAValidMove("e8", "d8");
+    boolean validMove = runner.isAValidMove("h3", "h2");
 
     assertThat(validMove, is(false));
   }
 
   @Test
-  public void Runner_isAllowedToMoveOneDiagonal_validMove() throws Exception {
-    Runner Runner = new Runner();
+  public void runner_isAllowedToMoveOneRight_invalidMove() throws Exception {
+    Runner runner = new Runner();
 
-    boolean validMove = Runner.isAValidMove("a2", "b3");
+    boolean validMove = runner.isAValidMove("e5", "f5");
+
+    assertThat(validMove, is(false));
+  }
+
+  @Test
+  public void runner_isAllowedToMoveOneLeft_invalidMove() throws Exception {
+    Runner runner = new Runner();
+
+    boolean validMove = runner.isAValidMove("e8", "d8");
+
+    assertThat(validMove, is(false));
+  }
+
+  @Test
+  public void runner_isAllowedToMoveOneDiagonal_validMove() throws Exception {
+    Runner runner = new Runner();
+
+    boolean validMove = runner.isAValidMove("a2", "b3");
 
     assertThat(validMove, is(true));
   }
 
   @Test
-  public void Runner_isAllowedToMoveSevenDiagonal_validMove() throws Exception {
-    Runner Runner = new Runner();
+  public void runner_isAllowedToMoveSevenDiagonal_validMove() throws Exception {
+    Runner runner = new Runner();
 
-    boolean validMove = Runner.isAValidMove("a8", "h1");
+    boolean validMove = runner.isAValidMove("a8", "h1");
 
     assertThat(validMove, is(true));
   }
 
   @Test
-  public void Runner_isAllowedToMoveSevenUp_invalidMove() throws Exception {
-    Runner Runner = new Runner();
+  public void runner_isAllowedToMoveSevenUp_invalidMove() throws Exception {
+    Runner runner = new Runner();
 
-    boolean validMove = Runner.isAValidMove("a1", "a8");
-
-    assertThat(validMove, is(false));
-  }
-
-  @Test
-  public void Runner_isAllowedToMoveTwoLeft_invalidMove() throws Exception {
-    Runner Runner = new Runner();
-
-    boolean validMove = Runner.isAValidMove("h5", "a5");
+    boolean validMove = runner.isAValidMove("a1", "a8");
 
     assertThat(validMove, is(false));
   }
 
   @Test
-  public void Runner_isAllowedToMoveWetherHorizontalVerticalOrDiagonal_invalidMove() throws Exception {
-    Runner Runner = new Runner();
+  public void runner_isAllowedToMoveTwoLeft_invalidMove() throws Exception {
+    Runner runner = new Runner();
 
-    boolean validMove = Runner.isAValidMove("c2", "d4");
+    boolean validMove = runner.isAValidMove("h5", "a5");
+
+    assertThat(validMove, is(false));
+  }
+
+  @Test
+  public void runner_isAllowedToMoveWetherHorizontalVerticalOrDiagonal_invalidMove() throws Exception {
+    Runner runner = new Runner();
+
+    boolean validMove = runner.isAValidMove("c2", "d4");
 
     assertThat(validMove, is(false));
   }
--- Bi-Chess/src_test/tech/bison/trainee2017/chess/TowerTest.java
+++ Bi-Chess/src_test/tech/bison/trainee2017/chess/TowerTest.java
@@ -7,82 +7,82 @@ import org.junit.Test;
 
 public class TowerTest {
   @Test
-  public void Tower_isAllowedToMoveOneUp_validMove() throws Exception {
-    Tower Tower = new Tower();
+  public void tower_isAllowedToMoveOneUp_validMove() throws Exception {
+    Tower tower = new Tower();
 
-    boolean validMove = Tower.isAValidMove("a2", "a3");
-
-    assertThat(validMove, is(true));
-  }
-
-  @Test
-  public void Tower_isAllowedToMoveOneDown_validMove() throws Exception {
-    Tower Tower = new Tower();
-
-    boolean validMove = Tower.isAValidMove("c3", "c2");
+    boolean validMove = tower.isAValidMove("a2", "a3");
 
     assertThat(validMove, is(true));
   }
 
   @Test
-  public void Tower_isAllowedToMoveOneRight_validMove() throws Exception {
-    Tower Tower = new Tower();
+  public void tower_isAllowedToMoveOneDown_validMove() throws Exception {
+    Tower tower = new Tower();
 
-    boolean validMove = Tower.isAValidMove("e5", "f5");
-
-    assertThat(validMove, is(true));
-  }
-
-  @Test
-  public void Tower_isAllowedToMoveOneLeft_validMove() throws Exception {
-    Tower Tower = new Tower();
-
-    boolean validMove = Tower.isAValidMove("e8", "d8");
+    boolean validMove = tower.isAValidMove("c3", "c2");
 
     assertThat(validMove, is(true));
   }
 
   @Test
-  public void Tower_isAllowedToMoveOneDiagonal_invalidMove() throws Exception {
-    Tower Tower = new Tower();
+  public void tower_isAllowedToMoveOneRight_validMove() throws Exception {
+    Tower tower = new Tower();
 
-    boolean validMove = Tower.isAValidMove("a2", "b3");
+    boolean validMove = tower.isAValidMove("e5", "f5");
+
+    assertThat(validMove, is(true));
+  }
+
+  @Test
+  public void tower_isAllowedToMoveOneLeft_validMove() throws Exception {
+    Tower tower = new Tower();
+
+    boolean validMove = tower.isAValidMove("e8", "d8");
+
+    assertThat(validMove, is(true));
+  }
+
+  @Test
+  public void tower_isAllowedToMoveOneDiagonal_invalidMove() throws Exception {
+    Tower tower = new Tower();
+
+    boolean validMove = tower.isAValidMove("a2", "b3");
 
     assertThat(validMove, is(false));
   }
 
   @Test
-  public void Tower_isAllowedToMoveSevenDiagonal_invalidMove() throws Exception {
-    Tower Tower = new Tower();
+  public void tower_isAllowedToMoveSevenDiagonal_invalidMove() throws Exception {
+    Tower tower = new Tower();
 
-    boolean validMove = Tower.isAValidMove("a1", "h8");
+    boolean validMove = tower.isAValidMove("a1", "h8");
 
     assertThat(validMove, is(false));
   }
 
   @Test
-  public void Tower_isAllowedToMoveSevenUp_validMove() throws Exception {
-    Tower Tower = new Tower();
+  public void tower_isAllowedToMoveSevenUp_validMove() throws Exception {
+    Tower tower = new Tower();
 
-    boolean validMove = Tower.isAValidMove("d1", "d8");
-
-    assertThat(validMove, is(true));
-  }
-
-  @Test
-  public void Tower_isAllowedToMoveTwoLeft_validMove() throws Exception {
-    Tower Tower = new Tower();
-
-    boolean validMove = Tower.isAValidMove("h8", "a8");
+    boolean validMove = tower.isAValidMove("d1", "d8");
 
     assertThat(validMove, is(true));
   }
 
   @Test
-  public void Tower_isAllowedToMoveWetherHorizontalVerticalOrDiagonal_invalidMove() throws Exception {
-    Tower Tower = new Tower();
+  public void tower_isAllowedToMoveTwoLeft_validMove() throws Exception {
+    Tower tower = new Tower();
 
-    boolean validMove = Tower.isAValidMove("c2", "d4");
+    boolean validMove = tower.isAValidMove("h8", "a8");
+
+    assertThat(validMove, is(true));
+  }
+
+  @Test
+  public void tower_isAllowedToMoveWetherHorizontalVerticalOrDiagonal_invalidMove() throws Exception {
+    Tower tower = new Tower();
+
+    boolean validMove = tower.isAValidMove("c2", "d4");
 
     assertThat(validMove, is(false));
   }
