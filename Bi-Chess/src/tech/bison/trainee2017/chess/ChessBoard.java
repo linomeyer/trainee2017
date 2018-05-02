9e2106b464da097d5723076d3f5bc86dac6d592c
Luca Bucher <lucbu01@bluewin.ch>
2018-05-02 12:12:18 +0200

    started board

--- Bi-Chess/src/tech/bison/trainee2017/chess/ChessBoard.java
+++ Bi-Chess/src/tech/bison/trainee2017/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package tech.bison.trainee2017.chess;
 
+import java.util.HashMap;
+
 import tech.bison.trainee2017.chess.Figure.colors;
 
 public class ChessBoard {
@@ -10,22 +12,62 @@ public class ChessBoard {
   }
 
   public void initializeBoard() {
-    Object[][] board = {
+    try {
 
-        { new Tower(colors.WHITE), "A1" }, { new Tower(colors.WHITE), "H1" }, { new Tower(colors.BLACK), "A8" },
-        { new Tower(colors.BLACK), "H8" },
-        { "H", "B1" }, { "H", "G1" }, { "H", "G8" }, { "H", "H8" },
-        { "R", "C1" }, { "R", "F1" }, { "R", "C8" }, { "R", "F8" },
+      HashMap<Position, Figure> board = new HashMap<Position, Figure>();
+      board.put(new Position("A1"), new Tower(colors.WHITE));
+      board.put(new Position("H1"), new Tower(colors.WHITE));
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
+      board.put();
 
-        { "K", "E1" }, { "K", "E8" },
-        { "Q", "D1" }, { "Q", "D8" },
+      Object[][] old_board = {
 
-        { "F", "A2" }, { "F", "B2" }, { "F", "C2" }, { "F", "D2" },
-        { "F", "E2" }, { "F", "F2" }, { "F", "G2" }, { "F", "H2" },
+          { new Tower(colors.WHITE), new Position("A1") }, { new Tower(colors.WHITE), new Position("H1") },
+          { new Tower(colors.BLACK), new Position("A8") }, { new Tower(colors.BLACK), new Position("H8") },
+          { new Horse(colors.WHITE), new Position("B1") }, { new Horse(colors.WHITE), new Position("G1") },
+          { "H", "G8" }, { "H", "H8" },
+          { "R", "C1" }, { "R", "F1" }, { "R", "C8" }, { "R", "F8" },
 
-        { "F", "A7" }, { "F", "B7" }, { "F", "C7" }, { "F", "D7" },
-        { "F", "E7" }, { "F", "F7" }, { "F", "G7" }, { "F", "H7" },
-    };
-    this.board = board;
+          { "K", "E1" }, { "K", "E8" },
+          { "Q", "D1" }, { "Q", "D8" },
+
+          { "F", "A2" }, { "F", "B2" }, { "F", "C2" }, { "F", "D2" },
+          { "F", "E2" }, { "F", "F2" }, { "F", "G2" }, { "F", "H2" },
+
+          { "F", "A7" }, { "F", "B7" }, { "F", "C7" }, { "F", "D7" },
+          { "F", "E7" }, { "F", "F7" }, { "F", "G7" }, { "F", "H7" },
+      };
+
+      this.board = old_board;
+    } catch (InvalidPositionException e) {
+    }
   }
 }
