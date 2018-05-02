35f58217b99e00dded778c319ff8619a0226b5e5
Lino Meyer <linomeyer02@gmail.com>
2018-05-02 11:56:45 +0200

    stated adding figures to board

--- Bi-Chess/src/tech/bison/trainee2017/chess/ChessBoard.java
+++ Bi-Chess/src/tech/bison/trainee2017/chess/ChessBoard.java
@@ -1,27 +1,28 @@
 package tech.bison.trainee2017.chess;
 
+import tech.bison.trainee2017.chess.Figure.colors;
+
 public class ChessBoard {
-  public String[][] board;
+  public Object[][] board;
 
   public ChessBoard() {
     initializeBoard();
   }
 
   public void initializeBoard() {
-    String[][] board = {
-        /*- White                     Black-*/
-        { "T", "A1" }, { "T", "H1" }, { "T", "A8" }, { "T", "H8" },
+    Object[][] board = {
+
+        { new Tower(colors.WHITE), "A1" }, { new Tower(colors.WHITE), "H1" }, { new Tower(colors.BLACK), "A8" },
+        { new Tower(colors.BLACK), "H8" },
         { "H", "B1" }, { "H", "G1" }, { "H", "G8" }, { "H", "H8" },
         { "R", "C1" }, { "R", "F1" }, { "R", "C8" }, { "R", "F8" },
 
-        /*- White      Black-*/
         { "K", "E1" }, { "K", "E8" },
         { "Q", "D1" }, { "Q", "D8" },
 
-        // White
         { "F", "A2" }, { "F", "B2" }, { "F", "C2" }, { "F", "D2" },
         { "F", "E2" }, { "F", "F2" }, { "F", "G2" }, { "F", "H2" },
-        // Black
+
         { "F", "A7" }, { "F", "B7" }, { "F", "C7" }, { "F", "D7" },
         { "F", "E7" }, { "F", "F7" }, { "F", "G7" }, { "F", "H7" },
     };
