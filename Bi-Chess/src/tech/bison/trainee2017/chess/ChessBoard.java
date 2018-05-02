c0166d0f21c27d7d5e76e7869961ed6d1f231e5f
Luca Bucher <lucbu01@bluewin.ch>
2018-05-02 12:20:10 +0200

    Added all figures expect farmers to board

--- Bi-Chess/src/tech/bison/trainee2017/chess/ChessBoard.java
+++ Bi-Chess/src/tech/bison/trainee2017/chess/ChessBoard.java
@@ -17,20 +17,25 @@ public class ChessBoard {
       HashMap<Position, Figure> board = new HashMap<Position, Figure>();
       board.put(new Position("A1"), new Tower(colors.WHITE));
       board.put(new Position("H1"), new Tower(colors.WHITE));
-      board.put();
-      board.put();
-      board.put();
-      board.put();
-      board.put();
-      board.put();
-      board.put();
-      board.put();
-      board.put();
-      board.put();
-      board.put();
-      board.put();
-      board.put();
-      board.put();
+      board.put(new Position("A8"), new Tower(colors.BLACK));
+      board.put(new Position("H8"), new Tower(colors.BLACK));
+
+      board.put(new Position("B1"), new Horse(colors.WHITE));
+      board.put(new Position("G1"), new Horse(colors.WHITE));
+      board.put(new Position("B8"), new Horse(colors.BLACK));
+      board.put(new Position("G8"), new Horse(colors.BLACK));
+
+      board.put(new Position("C1"), new Runner(colors.WHITE));
+      board.put(new Position("F1"), new Runner(colors.WHITE));
+      board.put(new Position("C8"), new Runner(colors.BLACK));
+      board.put(new Position("F8"), new Runner(colors.BLACK));
+
+      board.put(new Position("D1"), new Queen(colors.WHITE));
+      board.put(new Position("D8"), new Queen(colors.BLACK));
+
+      board.put(new Position("E1"), new King(colors.WHITE));
+      board.put(new Position("E8"), new King(colors.BLACK));
+
       board.put();
       board.put();
       board.put();
