4c6ec21b77d69ddbc89cd45826d62edbf8937c09
Lino Meyer <linomeyer02@gmail.com>
2018-05-04 15:40:45 +0200

    splitted board initialization

--- Bi-Chess/src/tech/bison/trainee2017/chess/ChessBoard.java
+++ Bi-Chess/src/tech/bison/trainee2017/chess/ChessBoard.java
@@ -5,36 +5,60 @@ import java.util.HashMap;
 import tech.bison.trainee2017.chess.Piece.colors;
 
 public class ChessBoard {
-  public HashMap<Square, Piece> board;
+  public HashMap<Square, Piece> whiteSideOfBoard;
+  public HashMap<Square, Piece> blackSideOfBoard;
 
   public ChessBoard() {
-    initializeBoard();
+    initializeWhiteSideOfBoard();
+    initializeBlackSideOfBoard();
   }
 
-  public void initializeBoard() {
+  public void initializeBlackSideOfBoard() {
+    HashMap<Square, Piece> board = new HashMap<Square, Piece>();
 
     try {
-      HashMap<Square, Piece> board = new HashMap<Square, Piece>();
-      board.put(new Square("A1"), new Rook(colors.WHITE));
-      board.put(new Square("H1"), new Rook(colors.WHITE));
       board.put(new Square("A8"), new Rook(colors.BLACK));
       board.put(new Square("H8"), new Rook(colors.BLACK));
 
-      board.put(new Square("B1"), new Knight(colors.WHITE));
-      board.put(new Square("G1"), new Knight(colors.WHITE));
       board.put(new Square("B8"), new Knight(colors.BLACK));
       board.put(new Square("G8"), new Knight(colors.BLACK));
 
-      board.put(new Square("C1"), new Bishop(colors.WHITE));
-      board.put(new Square("F1"), new Bishop(colors.WHITE));
       board.put(new Square("C8"), new Bishop(colors.BLACK));
       board.put(new Square("F8"), new Bishop(colors.BLACK));
 
-      board.put(new Square("D1"), new Queen(colors.WHITE));
       board.put(new Square("D8"), new Queen(colors.BLACK));
 
-      board.put(new Square("E1"), new King(colors.WHITE));
       board.put(new Square("E8"), new King(colors.BLACK));
+
+      board.put(new Square("B7"), new BlackPawn());
+      board.put(new Square("C7"), new BlackPawn());
+      board.put(new Square("D7"), new BlackPawn());
+      board.put(new Square("E7"), new BlackPawn());
+      board.put(new Square("F7"), new BlackPawn());
+      board.put(new Square("G7"), new BlackPawn());
+      board.put(new Square("H7"), new BlackPawn());
+
+      blackSideOfBoard = board;
+    } catch (InvalidSquareException e) {
+    }
+  }
+
+  public void initializeWhiteSideOfBoard() {
+    HashMap<Square, Piece> board = new HashMap<Square, Piece>();
+
+    try {
+      board.put(new Square("A1"), new Rook(colors.WHITE));
+      board.put(new Square("H1"), new Rook(colors.WHITE));
+
+      board.put(new Square("B1"), new Knight(colors.WHITE));
+      board.put(new Square("G1"), new Knight(colors.WHITE));
+
+      board.put(new Square("C1"), new Bishop(colors.WHITE));
+      board.put(new Square("F1"), new Bishop(colors.WHITE));
+
+      board.put(new Square("D1"), new Queen(colors.WHITE));
+
+      board.put(new Square("E1"), new King(colors.WHITE));
 
       board.put(new Square("A2"), new WhitePawn());
       board.put(new Square("B2"), new WhitePawn());
@@ -45,17 +69,12 @@ public class ChessBoard {
       board.put(new Square("G2"), new WhitePawn());
       board.put(new Square("H2"), new WhitePawn());
       board.put(new Square("A7"), new WhitePawn());
-      board.put(new Square("B7"), new BlackPawn());
-      board.put(new Square("C7"), new BlackPawn());
-      board.put(new Square("D7"), new BlackPawn());
-      board.put(new Square("E7"), new BlackPawn());
-      board.put(new Square("F7"), new BlackPawn());
-      board.put(new Square("G7"), new BlackPawn());
-      board.put(new Square("H7"), new BlackPawn());
-
-      this.board = board;
+      whiteSideOfBoard = board;
     } catch (InvalidSquareException e) {
     }
+  }
+
+  public void initializeBoard() {
 
   }
 }
