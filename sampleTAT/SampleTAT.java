1d1f4bced31fdcbc76a8ab76e6094a6d7de8bcdd
Luca Bucher <lucbu01@bluewin.ch>
2018-06-20 14:46:06 +0200

    added sample

---
+++ sampleTAT/SampleTAT.java
@@ -0,0 +1,16 @@
+package tech.bison.trainee17.arbeitsbuch;
+
+public class SampleTAT {
+  public static void main(String[] args) {
+    System.out.println("🖥 Server eingeben: srv507");
+    System.out.println("\n[root]");
+    System.out.println("\tFranz");
+    System.out.println("\tReto");
+    System.out.println("\tSilvan");
+    System.out.println("\tFlorian");
+    System.out.println("\n[readonly]");
+    System.out.println("\tLars");
+    System.out.println("\tLino");
+    System.out.println("\tLuca");
+  }
+}
