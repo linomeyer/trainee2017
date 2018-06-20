bbaab083b303e98e2ac9d2fb9214456a978d256c
Luca Bucher <lucbu01@bluewin.ch>
2018-06-20 15:28:32 +0200

    added symbol for user

--- sampleTAT/src/tech/bison/trainee2017/sampleTAT/SampleTAT.java
+++ sampleTAT/src/tech/bison/trainee2017/sampleTAT/SampleTAT.java
@@ -3,12 +3,12 @@ package tech.bison.trainee2017.sampleTAT;
 public class SampleTAT {
   public static void main(String[] args) {
     System.out.println("🖥 Server eingeben: srv507");
-    System.out.println("\n[root]");
+    System.out.println("\n👤[root]");
     System.out.println("\tFranz");
     System.out.println("\tReto");
     System.out.println("\tSilvan");
     System.out.println("\tFlorian");
-    System.out.println("\n[readonly]");
+    System.out.println("\n👤[readonly]");
     System.out.println("\tLars");
     System.out.println("\tLino");
     System.out.println("\tLuca");
